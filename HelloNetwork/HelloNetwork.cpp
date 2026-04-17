b6d8f50b816bea5d7d07879a76ec81b2c279cd66
Vincenzo <dev.vincenzo2000@gmail.com>
2026-04-17 23:24:02 +0200

    Add files via upload

--- HelloNetwork/HelloNetwork.cpp
+++ HelloNetwork/HelloNetwork.cpp
@@ -13,16 +13,14 @@
 using namespace std;
 
 
-
 struct Target
 {
     string name;
     string ip;
     int port;
     bool isOpen = false;
+    string banner;
 };
-
-
 
 
 void Status(Target& target)
@@ -35,14 +33,31 @@ void Status(Target& target)
 
 
 
-
 void Info()
 {
     cout << "Usa: status, list, info, help, exit\nclose, disconnect, localhost\nrun, exploit, start\ncls, clear" << endl;
 }
 
 
-
+void LogResult(Target& target)
+{
+    ofstream file;
+    file.open("report.txt", ios::app);
+    
+    if (file.is_open()) {
+        file << "-----------------------------" << endl;
+        file << "TARGET: " << target.ip << endl;
+        file << "PORTA : " << target.port << endl;
+        file << "STATO : " << (target.isOpen ? "APERTA" : "CHIUSA") << endl;
+        file << "BANNER: " << target.banner << endl;
+        file << "-----------------------------" << endl;
+        
+        file.close();
+        cout << "[+] Risultato salvato in report.txt" << endl;
+    } else {
+        cout << "[!] Errore: Impossibile creare il file di log." << endl;
+    }
+}
 
 
 void Exploit(Target& target)
@@ -51,8 +66,7 @@ void Exploit(Target& target)
     ofstream logger;
     logger.open("log.txt", ios::app);
     
-    WSADATA info; 
-    WSAStartup(MAKEWORD(2,2), &info);
+    
     
     SOCKET mySocket = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -70,29 +84,50 @@ void Exploit(Target& target)
     cout << "[i] Tentativo di connessione..." << endl;
 
     int connessione = connect(mySocket, (SOCKADDR*)&service, sizeof(service));
+    
 
     if (connessione == SOCKET_ERROR) {
         cout << "[!] ERROR: Connessione fallita" << endl;
         target.isOpen = false;
-        logger << "Target: " << target.name << endl;
-        logger << "IP: " << target.ip << endl;
-        logger << "PORT: " << target.port << endl;
-        logger << "STATUS: CLOSED" << endl;
+        target.banner = "Nessuna Risposta (Chiusa)";
+        
+        logger << "TARGET: " << target.ip << endl;
+        logger << "PORTA : " << target.port << endl;
+        logger << "STATO : " << (target.isOpen ? "APERTA" : "CHIUSA") << endl;
+        logger << "BANNER: " << target.banner << endl;
+        
+        LogResult(target);
     } else {
+        
+        char buffer[1024] = {0};
+        
+        DWORD timeout = 2000;
+        setsockopt(mySocket, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout, sizeof(timeout));
+        
+        int bytesReceived = recv(mySocket, buffer, sizeof(buffer) - 1, 0);
+        
+        if (bytesReceived > 0)
+        {
+            buffer[bytesReceived] = '\0';
+            target.banner = string(buffer);
+        } else
+        {
+            target.banner = "Banner non riconosciuto";
+        }
+        
         target.isOpen = true;
-        cout << "[+] CONNESSO: Sei dentro al bersaglio" << endl;
-        target.isOpen = false;
-        logger << "Target: " << target.name << endl;
-        logger << "IP: " << target.ip << endl;
-        logger << "PORT: " << target.port << endl;
-        logger << "STATUS: OPEN" << endl;
+
+        logger << "TARGET: " << target.ip << endl;
+        logger << "PORTA : " << target.port << endl;
+        logger << "STATO : " << (target.isOpen ? "APERTA" : "CHIUSA") << endl;
+        logger << "BANNER: " << target.banner << endl;
+        
+        LogResult(target);
     }
     closesocket(mySocket); 
-    WSACleanup();
+    
     logger.close();
 }
-
-
 
 
 
@@ -121,8 +156,46 @@ void Change(Target& target)
 
 
 
+void FastScan(Target& target, int in, int fi)
+{
+    for (int i = in; i < fi; i++) 
+    {
+        SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
+        
+        DWORD timeout = 200;
+        setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, (const char*)&timeout, sizeof(timeout));
+
+        sockaddr_in service;
+        service.sin_family = AF_INET;
+        service.sin_port = htons(i);
+        service.sin_addr.s_addr = inet_addr(target.ip.c_str());
+        
+        int connessione = connect(s, (SOCKADDR*)&service, sizeof(service));
+
+        if (connessione != SOCKET_ERROR) 
+        {
+            cout << "[+] PORTA APERTA: " << i << endl;
+            
+            target.port = i;
+            target.isOpen = true;
+            target.banner = "Banner non richiesto"; 
+            
+            LogResult(target);
+        }
+        else 
+        {
+            cout << "[-] Porta " << i << " chiusa." << endl;
+        }
+        closesocket(s);
+    }
+    cout << "[i] Scansione completata." << endl;
+}
+
 
 int main() {
+    
+    WSADATA info; 
+    WSAStartup(MAKEWORD(2,2), &info);
     
     system("title System Logic Scanner");
 
@@ -130,11 +203,6 @@ int main() {
     string command;
     Target target = {"LocalHost", "127.0.0.1", 443};
     
-    
-   
-    
-    
-
     
     while(true)
     {
@@ -149,6 +217,14 @@ int main() {
         } else if (command == "change")
         {
             Change(target);
+        } else if (command == "scan") {
+            int in, fi;
+            cout << "Porta iniziale: ";
+            cin >> in;
+            cout << "Porta finale: ";
+            cin >> fi;
+            
+            FastScan(target, in, fi);
             
         } else if (command == "clear" || command == "cls")
         {
@@ -178,17 +254,19 @@ int main() {
             cout << "[!] Comando sconosciuto. Usa: list, info o help per visualizzare i comandi" << endl;
         }
     }
+    
+    
+    
+    int a = 10;
+    int b = 5;
 
+    if (a > b)
+    {
+        std::cout << "";
+    }
+    
+
+    WSACleanup();
     system("pause");
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
